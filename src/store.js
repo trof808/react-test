1544e40648f39b96cbd3eabe77b5e97670038849
trof <reatrof@gmail.com>
2017-05-12 11:29:09 +0300

    change store class on function store

--- src/store.js
+++ src/store.js
@@ -19,34 +19,3 @@ export default class Store {
     return () => this._callbacks = this._callbacks.filter(cb => cb !== callback);
   }
 }
-
-// function updateState(state, action) {
-//     if(action.type === 'INCREMENT') {
-//       return { count: state.count + action.count };
-//     } else if(action.type === 'DECREMENT') {
-//       return { count: state.count - action.count };
-//     } else {
-//       return state;
-//     }
-// }
-
-
-// const initialState = { count: 0 }
-//
-// const store = new Store(updateState, initialState);
-//
-// const incrementAction = {
-//   type: 'INCREMENT',
-//   count: 5
-// }
-//
-// const decrementAction = {
-//   type: 'DECREMENT',
-//   count: 3
-// }
-//
-// const unsubscribe = store.subscribe(() => {console.log('state changed', store.state)})
-//
-// store.update(incrementAction)
-// store.update(decrementAction)
-// store.update({});
